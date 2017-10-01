6e151a7877d6ea144d1320d7954da481d0d6a6a6
Team RetroDev <31925400+TeamRetroDev1@users.noreply.github.com>
2017-10-01 18:13:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 chmod +x $HOME/TeamRetroDev/update.sh
 7z x -y install.7z
+mkdir $HOME/.attract/layouts/Hyperspin/3do/Themes/
 cp *.mp4 $HOME/.attract/layouts/Media/Main\ Menu/Video/
 cp *.zip $HOME/.attract/layouts/Media/Main\ Menu/Themes/
 cp 3do.the $HOME/.attract/layouts/Hyperspin/3do/Themes/default.zip
@@ -9,12 +10,12 @@ cp idea.bak $HOME/.wgetrc
 rm *.bak
 rm *.mp4
 rm *.zip
-rm *.the
+#rm *.the
 sh wheel.sh
 7z x -y wheelngpc.7z -o$HOME/RetroPie/roms/ngpc/wheel
 7z x -y wheel3do.7z -o$HOME/RetroPie/roms/3do/wheel
 7z x -y wheelpsx.7z -o$HOME/RetroPie/roms/psx/wheel
-sed -i".bak" '2,17d' install.sh
+sed -i".bak" '2,18d' install.sh
 echo
 echo '*****************************************'
 echo '*  Installation des videos pour ARCADE  *'
